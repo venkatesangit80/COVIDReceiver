4561caeb7b075aeef6431542669f774b7dc201d1
venkatesanlalitha <venkatesanshivangi@gmail.com>
2020-04-10 09:26:03 -0500

    Graph and Table View - Patch Update

--- app.py
+++ app.py
@@ -9,38 +9,46 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/Details/<country>')
 def detail_data(country):
     covid_data_url = "https://pomber.github.io/covid19/timeseries.json"
     res_value = rq.get(covid_data_url, verify=False)
     data_res = res_value.json()
-    data_df = pandas.DataFrame.from_dict(data_res[country])
-    data_df['date1'] = pandas.to_datetime(data_df['date'])
-    data_df = data_df.sort_values(by='date1', ascending=False)
-    json_data = data_df.to_json(orient='records')
-    json_object = json.loads(json_data)
-    return render_template('Index.html', len=len(json_object), items=json_object)
-    #return json_data
+    if country in data_res:
+        data_df = pandas.DataFrame.from_dict(data_res[country])
+        data_df['date1'] = pandas.to_datetime(data_df['date'])
+        data_df = data_df.sort_values(by='date1', ascending=False)
+        json_data = data_df.to_json(orient='records')
+        json_object = json.loads(json_data)
+        return render_template('Index.html', len=len(json_object), items=json_object)
+    else:
+        return "Look into Country typo"
+
+
 @app.route('/Graph/<country>')
 def graph_data(country):
     #labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August']
     covid_data_url = "https://pomber.github.io/covid19/timeseries.json"
     res_value = rq.get(covid_data_url, verify=False)
     data_res = res_value.json()
-    data_df = pandas.DataFrame.from_dict(data_res[country])
-    label_values= data_df['date'].tolist()
-    confirmed = data_df['confirmed'].tolist()
-    labels = []
-    labels.append('January')
-    labels.append('February')
-    labels.append('March')
-    labels.append('April')
-    labels.append('May')
-    labels.append('June')
-    labels.append('July')
-    labels.append('August')
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('Graph.html', values=confirmed, labels=label_values)
+    if country in data_res:
+        data_df = pandas.DataFrame.from_dict(data_res[country])
+        label_values= data_df['date'].tolist()
+        confirmed = data_df['confirmed'].tolist()
+        labels = []
+        labels.append('January')
+        labels.append('February')
+        labels.append('March')
+        labels.append('April')
+        labels.append('May')
+        labels.append('June')
+        labels.append('July')
+        labels.append('August')
+        values = [10, 9, 8, 7, 6, 4, 7, 8]
+        return render_template('Graph.html', values=confirmed, labels=label_values)
+    else:
+        return "Look into Country typo"
 
 
 if __name__ == '__main__':
